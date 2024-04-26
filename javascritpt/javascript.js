6dc06398f1f1bec9afa44f73b5e2371ccdb66400
Vanship77 <v4nship77@gmail.com>
2024-04-26 11:30:02 -0500

    codigos javascript codigos en pagina codigos

---
+++ javascritpt/javascript.js
@@ -0,0 +1,74 @@
+var saludo="Hola mundo desde JavaScript"
+console.log(saludo)
+//comentarios
+
+/* 
+AQUI VA NUESTROS COMENTARIOS
+
+
+forma de escribir la sintaxis!!!!!!!    
+var nombre 
+var $nombre
+var _nombre
+var nombre1
+var nombre_estudiante
+
+
+como no se puede escribir una variable!!!!!!
+var 8nombre
+tampoco se puede ultilizar palabras reservadas de javascript
+var - if - while - case - for
+
+
+como creamos nuestros archivos en js???????
+JAVASCRIPT.js
+var holaMundo   
+def HolaMundo  {
+}
+
+/*-----------variables Javascript--------------
+EXITEN 2 TIPOS
+    1. var=> variable global que se puede utilizar en todo la vida del 
+    programa o todos los alcances
+    2. let=> variable privada que se utiliza en una parte especifica de mi programa 
+    eso quiere decie q es una variable local 
+
+    
+
+
+*/                                       
+{
+let hello = "hi world welcome to my introduction to javascript and now i dont know what it is ya tusá b "
+
+
+}
+
+var a
+    a=7
+    console.log( "el valor de a es",7)
+let b 
+b=6
+console.log("el valor de b es",6)
+
+console.log(window)
+console.log(window.a)
+console.log(window.b)
+
+/*-----------variables tipo var--------------*/
+
+var musica="Baladas"
+console.log("Variable antes del bloque",musica)
+{
+var  musica="Cumbia"
+console.log("Variable dentro del bloque",musica)
+
+}
+console.log("Variable fuera del bloque",musica)
+console.log("**********Let**********")
+let musica2="Salsa"
+console.log("variable antes de entrar al bloque ",musica2)
+{
+    let musica2="Merenge"
+    console.log("Variable dentro del bloque" ,musica2)
+}
+
